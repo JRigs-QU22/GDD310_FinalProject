c871417f2e451559efbca3ac498ee7f43d556cd2
JRigs-QU22 <jtrigsby@quinnipiac.edu>
2020-12-10 09:08:20 -0500

    Initial commit

---
+++ Source/GDD310_FinalProject2/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GDD310_FINALPROJECT2_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
